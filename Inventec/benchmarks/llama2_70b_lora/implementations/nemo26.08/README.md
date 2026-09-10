c419c779b3b278ceafe99532720c7a14af2b2c80
JK Jung <jkjung13@gmail.com>
2026-09-10 10:05:29 +0800

    Update llama2_70b_lora to support both P5800G7 and P9000G7

--- Inventec/benchmarks/llama2_70b_lora/implementations/nemo26.08/README.md
+++ Inventec/benchmarks/llama2_70b_lora/implementations/nemo26.08/README.md
@@ -72,10 +72,10 @@ The launch command structure:
 
 ```bash
 export CONT=/mnt/sqsh/llama2_70b_lora_nemo26.08.sqsh
-export LOGDIR=../../../../results/P9000IG7_nemo26.08/llama2_70b_lora
+export LOGDIR=../../../../results/P5800G7_nemo26.08/llama2_70b_lora
 export MODEL=/raid/data/mlperf_training/llama2_70b_lora/model
 export DATADIR=/raid/data/mlperf_training/llama2_70b_lora/gov_report
-source config_P9000IG7_1x8x1xtp1pp1cp1_fp4.sh
+source config_P5800G7_1x8x1xtp1pp1cp1_fp4.sh
 ```
 
 Launch the training job on a specific compute node (e.g. p5800-1):
---
+++ Inventec/benchmarks/llama2_70b_lora/implementations/nemo26.08/config_P5800G7_1x8x1xtp1pp1cp1_fp4.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source $(dirname ${BASH_SOURCE[0]})/config_common.sh
+source $(dirname ${BASH_SOURCE[0]})/config_common_fp4.sh
+
+# no munge in the docker container
+export PMIX_MCA_psec=^munge
+
+# hyperparameters
+export MAX_STEPS=550
+export LR=0.0006
+export MINIBS=1
+export CP=1
+export MCORE_CUDA_GRAPH=1
+export BUCKET_SIZE=10000000
+export NUM_WORKERS=8
+
+export HEALING_ITER=350
+
+# system parameters
+export VBOOST_VALUE=0
+export DGXNNODES=1
+export DGXNGPU=8
+export WALLTIME_RUNANDTIME=35
+export WALLTIME=$((5 + ${NEXP:-1} * ($WALLTIME_RUNANDTIME + 5)))
+
+export MLPERF_SUBMITTER="Inventec"
+export MLPERF_SUBMISSION_ORG="Inventec Corporation"
+export MLPERF_CLUSTER_NAME="Inventec AI Lab"
+export MLPERF_SYSTEM_NAME="P5800G7 8xB300"
+export MLPERF_SUBMISSION_PLATFORM="Inventec P5800IG7"
+export MLPERF_STATUS="research"
